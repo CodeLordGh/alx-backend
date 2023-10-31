0abada6a14d98c1c7213f5ea8c9023bc488b6db3
CodeLordGh <babohkossi228@icloud.com>
2023-10-31 12:56:28 +0000

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+babel = Babel(__name__)
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+
+app = Flask(__name__, config=Config)
+babel.init_app(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
